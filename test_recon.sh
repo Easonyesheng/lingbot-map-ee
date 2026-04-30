261a9e2c2fb13b40c851e15b5adecdd21bc97196
Easonyesheng <preacher@sjtu.edu.cn>
2026-04-30 12:38:13 +0800

    update:test

--- test_recon.sh
+++ test_recon.sh
@@ -3,7 +3,7 @@
  # @Author: Easonyesheng preacher@sjtu.edu.cn
  # @Date: 2026-04-24 17:18:33
  # @LastEditors: Easonyesheng preacher@sjtu.edu.cn
- # @LastEditTime: 2026-04-24 17:20:17
+ # @LastEditTime: 2026-04-30 12:02:57
  # @FilePath: /recon/ee_recon/third_party/lingbot-map-ee/lingbot_map/test_recon.sh
  # @Description: test recon of lingbot-map
 ### 
@@ -13,8 +13,16 @@
 #     --use_sdpa
 
 
+# # in comac
+# python demo.py \
+#     --model_path /opt/data/private/recon/ee_recon/third_party/ckpts/lingbot-map.pt \
+#     --image_folder /opt/data/private/recon/data/comac/.video_frames_cache/right1_every_2 \
+#     --use_sdpa
+
+
 # in comac
 python demo.py \
     --model_path /opt/data/private/recon/ee_recon/third_party/ckpts/lingbot-map.pt \
-    --image_folder /opt/data/private/recon/data/comac/.video_frames_cache/right1_every_2 \
+    --video_path /opt/data/private/recon/data/comac/global.mp4 \
+    --fps 5 \
     --use_sdpa
